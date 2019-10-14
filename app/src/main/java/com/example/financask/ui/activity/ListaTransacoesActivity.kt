4a6739261e6ec64ba8e797fad24b0eaf00bd1ee5
Daniel <daniel.roque2@fatec.sp.gov.br>
2019-10-14 18:03:15 -0300

    Adicionado as Activitys e inicando os testes com Kotlin com declaraç~pes de variaveis

---
+++ app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,24 @@
+package com.example.financask.ui.activity
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.example.financask.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Computador - R$:1500,00", "Mesa Sinuca - R$: 800,00")
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+        lista_transacoes_listview.setAdapter(arrayAdapter)
+
+
+
+    }
+
+
+}
