416947784676ce684439dcf880989080eaec4654
Daniel Roque <daniel.atdrroque05@gmail.com>
2019-10-15 01:00:17 -0300

    Configurando BaseAdapter para criar uma nova view

--- app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.financask.R
+import com.example.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
@@ -12,9 +13,9 @@ class ListaTransacoesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf("Computador - R$:1500,00", "Mesa Sinuca - R$: 800,00")
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
-        lista_transacoes_listview.setAdapter(arrayAdapter)
+        val transacoes = listOf("Comida - R$:20,00", "Economia - R$: 100,00")
+        var arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+        lista_transacoes_listview.setAdapter(ListaTransacoesAdapter(transacoes, this))
 
 //teste
 
