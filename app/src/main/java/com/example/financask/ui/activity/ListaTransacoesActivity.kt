5f3b3f903fe734ad23baaf3a9bb7c4d030edbe4f
Daniel <daniel.roque2@fatec.sp.gov.br>
2019-10-16 18:18:36 -0300

    Aperfeiçoamento AlertaDialog e refatoração do código na classe ListaTransações

--- app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,15 +1,26 @@
 package com.example.financask.ui.activity
 
+import android.app.DatePickerDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.financask.Extension.formataParaBrasileiro
 import com.example.financask.R
 import com.example.financask.model.Tipo
 import com.example.financask.model.Transacao
 import com.example.financask.ui.ResumoView
 import com.example.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
-import java.util.Calendar
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
@@ -19,6 +30,66 @@ class ListaTransacoesActivity : AppCompatActivity() {
         val transacoes: List<Transacao> = transacoesDeExemplo()
         configuraResumo(transacoes)
         configuraLista(transacoes)
+
+        lista_transacoes_adiciona_receita.setOnClickListener {
+            val view = window.decorView
+            val viewCriada =
+                LayoutInflater.from(this).inflate(R.layout.form_transacao,
+                    view as ViewGroup, false)
+
+            val ano = 2019
+            val mes = 9
+            val dia = 16
+
+            val hoje = Calendar.getInstance()
+            viewCriada.form_transacao_data.setText(hoje.formataParaBrasileiro())
+
+            viewCriada.form_transacao_data.setOnClickListener {
+                DatePickerDialog(this,
+                    DatePickerDialog.OnDateSetListener { view, ano, mes, dia ->
+                        var dataSelecionada = Calendar.getInstance()
+                        dataSelecionada.set(ano,mes,dia)
+                        viewCriada.form_transacao_data.setText(dataSelecionada.formataParaBrasileiro())
+                    }
+                    , ano, mes, dia).show()
+            }
+
+            val adapter =
+                ArrayAdapter.createFromResource(this, R.array.categorias_de_despesa,
+                    android.R.layout.simple_spinner_dropdown_item)
+            viewCriada.form_transacao_categoria.adapter = adapter
+
+            AlertDialog.Builder(this)
+                .setTitle(R.string.adiciona_receita)
+                .setPositiveButton("Adicionar"
+                ) { dialogInterface, i ->
+                    val valorEmTexto = viewCriada
+                        .form_transacao_valor.text.toString()
+                    val dataEmTexto = viewCriada
+                        .form_transacao_data.text.toString()
+                    val categoriaEmTexto = viewCriada.form_transacao_categoria.selectedItem.toString()
+
+                    val valor = BigDecimal(valorEmTexto)
+                    val formatoBrasileiro = SimpleDateFormat("dd/MM/yyy")
+                    val dataConvertida = formatoBrasileiro.parse(dataEmTexto)
+                    val data = Calendar.getInstance()
+                    data.time = dataConvertida
+
+                    val transacaoCriada = Transacao(
+                        tipo = Tipo.RECEITA,
+                        valor = valor,
+                        data = data,
+                        categoria = categoriaEmTexto
+                    )
+                    Toast.makeText(this, "${transacaoCriada.valor} - " +
+                            "${transacaoCriada.data.formataParaBrasileiro()} - ${transacaoCriada.categoria}" +
+                            "- ${transacaoCriada.tipo}", Toast.LENGTH_LONG).show()
+                }
+                .setNegativeButton("Cancelar", null)
+                .setView(viewCriada)
+                .show()
+        }
+
     }
 
     private fun configuraResumo(transacoes: List<Transacao>) {
